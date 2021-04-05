ff47e6b4c34d39e9647689f242781db3039a50fe
Cade <cadeburkett@Cades-MBP.attlocal.net>
2021-04-04 23:55:46 -0700

    created footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="navbar sticky-bottom navbar-light bg-light">
+            <h3 className="footerText">Michael Burkett</h3>
+        </footer>
+    )
+}
+
+export default Footer
